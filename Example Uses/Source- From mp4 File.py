d13425c6fc6998b1ee33ce88dfb87b66bb43433e
fmort <86053070+fmortgit@users.noreply.github.com>
2025-12-08 17:32:06 +0000

    Add files via upload

--- Example Uses/Source- From mp4 File.py
+++ Example Uses/Source- From mp4 File.py
@@ -3,6 +3,7 @@ import cv2
 import random
 import tkinter as tk
 from tkinter import filedialog
+from tqdm import tqdm
 
 
 def getFilePath():
@@ -21,27 +22,31 @@ cap = cv2.VideoCapture(filePath)
 fps = cap.get(cv2.CAP_PROP_FPS)
 frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")        # defines the .mp4 codec
 out = cv2.VideoWriter(f"newVid_{random.randint(1000,9999)}.mp4", fourcc, fps, (frameWidth, frameHeight), isColor=False)
 
 editor = CircleMozaicCreator(10)
 
-while True:
-    ret, frame = cap.read()
+with tqdm(total=frameCount, desc="Processing video", unit=" frames") as pbar:
+    while True:
+        ret, frame = cap.read()
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    inputImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        inputImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    outputImg = editor.render(inputImg)
+        outputImg = editor.render(inputImg)
 
-    out.write(outputImg)
+        out.write(outputImg)
 
-    if cv2.waitKey(1) == ord("q"):
-        break
-print("Done!")
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+        pbar.update()
+print("\nDone!", end="")
 
 # clean up the camera, recording and window objs
 cap.release()
