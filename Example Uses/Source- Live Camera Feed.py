4e442b58b2e5ee34af62ddc6a65e555d25156ff6
fmort <86053070+fmortgit@users.noreply.github.com>
2025-12-08 18:14:19 +0000

    Update Source- Live Camera Feed.py

--- Example Uses/Source- Live Camera Feed.py
+++ Example Uses/Source- Live Camera Feed.py
@@ -2,12 +2,20 @@ from CircleMozaicCreator import CircleMozaicCreator
 import cv2
 
 cap = cv2.VideoCapture(0)
-editor = CircleMozaicCreator(10)
+editor = CircleMozaicCreator(25)
 while True:
     ret, frame = cap.read()
 
     inputImg = cv2.flip(frame, 1)
+
+    dims = inputImg.shape
+    height, width = dims[0], dims[1]
+
+    inputImg = cv2.resize(inputImg, (1000,1000))
+
     outputImg = editor.render(inputImg)
+
+    outputImg = cv2.resize(outputImg, (width, height))
 
     cv2.imshow("Camera Feed", outputImg)
 
